8775ff46068c8e55fbf78d81048b5ed3ee1d7148
khooyenni <73241253+khooyenni@users.noreply.github.com>
2021-05-20 09:36:24 +0800

    Delete Price.java

--- Price.java
+++
@@ -1,9 +0,0 @@
-package Exercise4_6;
-
-interface Price {
-	
-	double totalPrice(double q); //heading method without body
-	
-	double totalPrice(double q, double d); //heading method without body
-
-}
